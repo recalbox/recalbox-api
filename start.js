7918dc9481112bf1f9c99c14baf270983f359dd3
neolao <contact@neolao.com>
2016-09-10 15:30:35 +0200

    Add HTTP router

--- start.js
+++ start.js
@@ -2,10 +2,17 @@
 
 let solfege = require("solfegejs");
 let ServerBundle = require("solfegejs-server");
+let RouterBundle = require("solfegejs-server-router");
 let RecalboxApiBundle = require("./lib/Bundle");
 
+// Initialize application
 let application = solfege.factory();
 application.addBundle(new ServerBundle);
+application.addBundle(new RouterBundle);
 application.addBundle(new RecalboxApiBundle);
 
+// Load configuration
+application.loadConfiguration(`${__dirname}/config/production.yml`);
+
+// Start the server
 application.start(["recalbox-api:start"]);
